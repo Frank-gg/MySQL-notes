002587657f6814fc7589a141f904020aa2f82fa9
Frank-gg <897938948@qq.com>
2020-10-13 20:12:11 +0800

    To give a 32-bit signed integer, you need to reverse the numbers on each bit of the integer.

---
+++ LeetCode exercise/Integer inversion/Integer inversion.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ret = 0;
+        while (x) {
+            int t = (unsigned)ret * 10;
+            if (t / 10 != ret)return 0;
+            ret = t + x % 10;
+            x /= 10;
+        }
+        return ret;
+    }
+};
