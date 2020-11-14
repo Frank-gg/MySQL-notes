81e2b4d4a0898a0934998d2cfc46f4fe7da5ba2c
Frank-gg <897938948@qq.com>
2020-11-14 21:43:01 +0800

    exercise 11 14

---
+++ C language exercise/Pro 11 14/test 11 14.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//描述一个学生
+//int main()
+//{
+//	struct Stu s2;
+//	Stu s1 = { "张三",20,"15454","男" };//局部变量
+//
+//	return 0;
+//}
+//struct 结构体关键字 Stu 结构体标签 struct Stu 结构体类型
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}s1,s2,s3;//s1 s2 s3是三个全局变量
+//typedef struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex[5];
+//}Stu;
+
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char* pc;
+//};
+//
+//int main()
+//{
+//	char arr[] = "hello world\n";
+//	struct T t = { "hehe",{100,'w',"hello world",3.14},arr };
+//	printf("%s\n", t.ch);//hehe
+//	printf("%s\n", t.s.arr);//hello world
+//	printf("%lf\n", t.s.d);//3.14
+//
+//	return 0;
+//}
+
